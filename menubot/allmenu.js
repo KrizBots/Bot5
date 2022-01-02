eb00e337939aa7585ee4867be37fce4c011ee258
KrizBots <krizfuncion@gmail.com>
2022-01-01 19:42:12 -0500

    iniciando...

---
+++ menubot/allmenu.js
@@ -0,0 +1,191 @@
+const menunya = (prefix) => {
+return`━━֍  〔 _*BY ${botName}*_ 〕
+──────────────────
+◈ _Creador : ${NamaOwner}_
+◈ _Numero : ${NomorOwner}_
+────────────────────
+────────────────────
+━━֍〔 *MENU CREADOR* 〕
+────────────────────
+◈ ${prefix}serbot
+◈ ${prefix}autorespon
+◈ ${prefix}antidelete
+◈ ${prefix}bc [ txtx ]
+◈ ${prefix}tobc [  sticker ]
+◈ ${prefix}return [ javascript ]
+◈ ${prefix}clearall
+◈ ${prefix}delchat
+◈ ${prefix}mute
+◈ ${prefix}unmute
+◈ ${prefix}public
+◈ ${prefix}self
+◈ ${prefix}spam [ txt|cantidad]
+◈ ${prefix}demoteall
+◈ ${prefix}promoteall
+◈ ${prefix}spamsw [ teks|jumlah ]
+◈ ${prefix}upswteks [ teks ]
+◈ ${prefix}upswlokasi [ teks ]
+◈ ${prefix}upswaudio [ reply audio ]
+◈ ${prefix}upswvoice [ reply audio ]
+◈ ${prefix}upswsticker [ reply sticker ]
+◈ ${prefix}upswimage [ reply image ]
+◈ ${prefix}upswgif [ reply gif ]
+◈ ${prefix}upswvideo [ reply video ]
+◈ ${prefix}shutdown
+◈ ${prefix}offline [ alasan ]
+◈ ${prefix}online
+◈ ${prefix}exif [ nama|author ]
+◈ ${prefix}setprofile [ reply image ]
+◈ ${prefix}setname [ teks ]
+◈ ${prefix}setprefix [ multi/nopref/prefix ]
+◈ ${prefix}setbio [ teks ]
+◈ ${prefix}leave
+◈ ${prefix}restart
+◈ ${prefix}addrespon [ tanya|jawab ]
+◈ ${prefix}delrespon [ nama ]
+◈ ${prefix}listrespon
+◈ ${prefix}readall
+◈ ${prefix}unreadall
+◈ ${prefix}archive
+◈ ${prefix}unarchiveall
+◈ ${prefix}pin
+◈ ${prefix}unpin
+◈ ${prefix}setmenu [ ori/simpel ]
+◈ ${prefix}setallmenu [ ori/simpel ]
+◈ ${prefix}leavetime [ detik/menit/jam ]
+◈ ${prefix}bukatime [ detik/menit/jam ]
+◈ ${prefix}tutuptime [ detik/menit/jam ]
+◈ ${prefix}nano [ nama file ]
+────────────────────
+━━֍〔 *MENU DESCARGAS* 〕
+────────────────────
+◈ ${prefix}infogempa
+◈ ${prefix}herolist
+◈ ${prefix}herodetail [ hero ]
+◈ ${prefix}google [ search ]
+◈ ${prefix}gimage [ search ]
+◈ ${prefix}wiki [ search ]
+◈ ${prefix}mediafire [ link ]
+◈ ${prefix}ytmp4 [ link yt ]
+◈ ${prefix}ytmp3 [ link yt ]
+◈ ${prefix}play [ judul lagu ]
+◈ ${prefix}video [ judul video ]
+◈ ${prefix}tinyurl [ link ]
+◈ ${prefix}fetch [ link ]
+◈ ${prefix}igdl [ link ]
+◈ ${prefix}tiktokdl [ link ]
+◈ ${prefix}pinterest [ search ]
+◈ ${prefix}lirik [ judul ]
+◈ ${prefix}tourl [ reply image/video ]
+◈ ${prefix}resepmasakan [ judul ]
+◈ ${prefix}artimimpi [ teks ]
+◈ ${prefix}bilangangka [ angka ]
+◈ ${prefix}kalkulator [ angka ]
+◈ ${prefix}fancytext [ teks ]
+◈ ${prefix}githubstalk [ username ]
+◈ ${prefix}translate [ teks kodebhs ]
+◈ ${prefix}ss [ link ]
+────────────────────
+━━֍〔 *MENU LOGOS* 〕
+────────────────────
+◈ ${prefix}nulis 「 text 」
+◈ ${prefix}tahta 「 text 」
+◈ ${prefix}transformer 「 text 」
+◈ ${prefix}epepserti 「 text 」
+◈ ${prefix}logoff 「 text 」
+◈ ${prefix}maker3d 「 text 」
+◈ ${prefix}maker2d 「 text 」
+◈ ${prefix}cslogo 「 text 」
+◈ ${prefix}matrix 「 text 」
+◈ ${prefix}skytext 「 text 」
+◈ ${prefix}barcode 「 text 」
+◈ ${prefix}naruto 「 text 」
+◈ ${prefix}bneon 「 text 」
+◈ ${prefix}pubg 「 text 」
+◈ ${prefix}snow 「 text 」
+◈ ${prefix}cans 「 text 」
+◈ ${prefix}ganz 「 text 」
+◈ ${prefix}tfire 「 text 」
+◈ ${prefix}lithg 「 text 」
+◈ ${prefix}wall 「 text 」
+◈ ${prefix}dropair 「 text 」
+◈ ${prefix}wolf 「 text 」
+◈ ${prefix}flower 「 text 」
+◈ ${prefix}flame 「 text 」
+◈ ${prefix}gplay 「 text 」
+◈ ${prefix}splay 「 text 」
+────────────────────
+━━֍〔 *MENU GRUPOS* 〕
+◈ ${prefix}getpict [ @tag ]
+◈ ${prefix}getname [ reply target ]
+◈ ${prefix}getbio [ reply target ]
+◈ ${prefix}promote [ @tag ]
+◈ ${prefix}demote [ @tag ]
+◈ ${prefix}antilink [ on/off ]
+◈ ${prefix}antivirtex [ on/off ]
+◈ ${prefix}antiwame [ on/off ]
+◈ ${prefix}antibug [ on/off ]
+◈ ${prefix}welcome [ on/off ]
+◈ ${prefix}creategrup [ nama|@tag ]
+◈ ${prefix}tictactoe [ @tag ]
+◈ ${prefix}delttt
+◈ ${prefix}getpp
+◈ ${prefix}antiwame
+◈ ${prefix}getdeskgc
+◈ ${prefix}sider [ reply pesan bot ]
+◈ ${prefix}hacked [ teks ]
+◈ ${prefix}setnamegc [ teks ]
+◈ ${prefix}setdeskgc [ teks ]
+◈ ${prefix}fitnah [ @tag|teks1|teks2 ]
+◈ ${prefix}kontak [ @tag|nama ]
+◈ ${prefix}kontag [ @tag|nama ]
+◈ ${prefix}opengc
+◈ ${prefix}closegc
+◈ ${prefix}resetlinkgc
+◈ ${prefix}linkgrup
+◈ ${prefix}hidetag [ teks ]
+◈ ${prefix}sticktag [ nama sticker ]
+◈ ${prefix}totag [ reply media ]
+────────────────────
+━━֍〔 *OTROS MENU* 〕
+────────────────────
+◈ ${prefix}caripesan [ teks|jumlah ]
+◈ ${prefix}slots
+◈ ${prefix}suit [ gunting/batu/kertas ]
+◈ ${prefix}tag [ nomor ]
+◈ ${prefix}tagme
+◈ ${prefix}tts [ kodebhs teks ]
+◈ ${prefix}readmore [ teks1|teks2 ]
+◈ ${prefix}fitnahpc [ nomor|teks1|teks2 ]
+◈ ${prefix}chat [ nomor|teks ]
+◈ ${prefix}fdeface [ replyimg link|teks1|teks2 ]
+◈ ${prefix}listimage
+◈ ${prefix}liststicker
+◈ ${prefix}listvn
+◈ ${prefix}listgrup
+◈ ${prefix}baileys [ reply message ]
+◈ ${prefix}q [ reply message ]
+◈ ${prefix}getcaption [ reply message ]
+◈ ${prefix}pantun
+◈ ${prefix}tospam [ reply sticker|jumlah ]
+◈ ${prefix}sharelock [ teks1|teks2 ]
+◈ ${prefix}sticker
+◈ ${prefix}stickerwm [ nama|author ]
+◈ ${prefix}takestick [ nama|author ]
+◈ ${prefix}colong [ reply sticker ]
+◈ ${prefix}dadu
+◈ ${prefix}semoji [ emoji ]
+◈ ${prefix}attp [ teks ]
+◈ ${prefix}toimg
+◈ ${prefix}tomp3 [ reply video ]
+◈ ${prefix}tomp4 [ reply sticker gif ]
+◈ ${prefix}robot [ reply audio ]
+◈ ${prefix}balik [ reply audio ]
+◈ ${prefix}bass [ reply audio ]
+◈ ${prefix}gemuk [ reply audio ]
+◈ ${prefix}detikvn [ tag angka ]
+◈ ${prefix}detikvideo [ tag angka ]
+◈ ${prefix}anime
+────────────────────`
+}
+exports.menunya = menunya
